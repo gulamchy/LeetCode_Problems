ba72c2b2dbaf690fcec33ba95f511a5dede8bc28
gulamchy <gulamsulaman@gmail.com>
2024-01-02 17:58:14 -0800

    Leetcode Tree Problems

--- Tree/binaryInorderTraversal.py
+++ Tree/binaryInorderTraversal.py
@@ -6,12 +6,25 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # iterative
         res = []
-        def inorder(root):
-            if not root:
-                return
-            inorder(root.left)
-            res.append(root.val)
-            inorder(root.right)
-        inorder(root)
-        return res
+        stack = []
+        cur = root
+        while cur or stack:
+            while cur:
+                stack.append(cur.val)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+            
+        # Recursion
+        # res = []
+        # def inorder(root):
+        #     if not root:
+        #         return
+        #     inorder(root.left)
+        #     res.append(root.val)
+        #     inorder(root.right)
+        # inorder(root)
+        # return res
